73810d7ad7648f3b27f3198e3d8d1d8d1d600bc6
florian <duceptflorian53@gmail.com>
2020-06-27 09:12:59 +0200

    first commit

---
+++ webpack.dev.js
@@ -0,0 +1,22 @@
+const merge = require("webpack-merge");
+const common = require("./webpack.common");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = merge(common, {
+  mode: "development",
+  output: {
+    filename: "[name].bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+});
