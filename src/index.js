1a65bb37138229337ab5b9f8f1e19fbb0a8afa08
florian <duceptflorian53@gmail.com>
2020-07-02 14:14:26 +0200

    inital commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-alert("hello");
+import "bootstrap";
 import "./scss/main.scss";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
